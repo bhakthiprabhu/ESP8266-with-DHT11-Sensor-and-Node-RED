f224dcbe0ac72ada1a73408961cc62393a678ec7
Bhakthi Prabhu <101608694+bhakthiprabhu@users.noreply.github.com>
2022-09-22 22:03:29 +0530

    First Commit

---
+++ p1.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+const char *wifissid = "slpg5";
+const char *wifipass = "9848038171@5";
+void setup() 
+ {
+   // put your setup code here, to run once:
+   Serial.begin(115200);
+ }
+void loop() 
+ {
+   // put your main code here, to run repeatedly:
+    delay(1000);
+    reconnectWiFi();    
+ }
+ void reconnectWiFi()
+ {
+   WiFi.mode(WIFI_STA);
+   delay(200);
+   WiFi.begin(wifissid,wifipass);
+   while(WiFi.status()!= WL_CONNECTED)
+   {
+     Serial.print(".");
+     delay(500);
+   }
+ Serial.println("Connected to: \t");
+ Serial.println(WiFi.localIP());
+ }
+
+
+
+
+
+ 
